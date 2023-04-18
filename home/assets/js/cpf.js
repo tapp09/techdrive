9693b78122784f073a1541a95e3c1c23dc6af79c
tapp09 <caio_r_rosa@estudante.sesisenai.org.br>
2023-04-18 07:56:59 -0300

    opa

--- home/assets/js/cpf.js
+++ home/assets/js/cpf.js
@@ -12,3 +12,18 @@ function mascara(i){
     if (v.length == 11) i.value += "-";
  
  }
+
+ /* var v = i.value;: essa linha cria uma variável chamada v e atribui o valor atual do campo de entrada i a ela.
+
+if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número: essa linha verifica se o último caractere digitado no campo i é um número. Se não for, a linha seguinte é executada.
+
+i.value = v.substring(0, v.length-1);: essa linha remove o último caractere digitado do campo i.
+
+return;: essa linha interrompe a execução da função, uma vez que um caractere não-numérico foi digitado.
+
+i.setAttribute("maxlength", "14");: essa linha define o comprimento máximo do campo i como 14 caracteres.
+
+if (v.length == 3 || v.length == 7) i.value += ".";: essa linha adiciona um ponto (.) no campo i após o terceiro e o sétimo caracteres digitados.
+
+if (v.length == 11) i.value += "-";: essa linha adiciona um hífen (-) no campo i após o décimo primeiro caractere digitado.
+*/
--- home/assets/js/efeito-home-brilho.js
+++ home/assets/js/efeito-home-brilho.js
@@ -27,3 +27,47 @@
         }
         animacaoH1()
         animacaoH2()
+
+        /* var i = 0; cria uma variável chamada i e atribui a ela o valor inicial de 0.
+
+        var texto =" Ao Futuro"; cria uma variável chamada texto e atribui a ela a string "Ao Futuro".
+
+        var vel = 250 cria uma variável chamada vel e atribui a ela o valor 250.
+
+        var H1 = document.querySelector('h1') cria uma variável chamada H1 e atribui a ela o elemento HTML h1 encontrado na página da web usando o método document.querySelector().
+
+        function animacaoH1 () {...} define uma função chamada animacaoH1, que é usada para animar o texto no elemento h1.
+
+        if(i<texto.length){...} verifica se o valor atual da variável i é menor que o comprimento da string texto.
+
+        H1.innerHTML += texto.charAt(i) adiciona o caractere atual da string texto ao HTML do elemento h1.
+
+        i++ incrementa a variável i em 1.
+
+        setTimeout(animacaoH1, vel) define um atraso de vel milissegundos antes de chamar a função animacaoH1 novamente.
+
+        H1.classList.add('sombra') adiciona a classe sombra ao elemento h1 quando toda a string texto foi animada.
+
+        var a = 0; cria uma variável chamada a e atribui a ela o valor inicial de 0.
+
+        var texto2 =" O futuro chegou e é elétrico - junte-se a nós nessa jornada sustentável."; cria uma variável chamada texto2 e atribui a ela a string "O futuro chegou e é elétrico - junte-se a nós nessa jornada sustentável.".
+
+        var velo = 50 cria uma variável chamada velo e atribui a ela o valor 50.
+
+        var P = document.querySelector('p') cria uma variável chamada P e atribui a ela o elemento HTML p encontrado na página da web usando o método document.querySelector().
+
+        function animacaoH2 () {...} define uma função chamada animacaoH2, que é usada para animar o texto no elemento p.
+
+        if(a<texto2.length){...} verifica se o valor atual da variável a é menor que o comprimento da string texto2.
+
+        P.innerHTML += texto2.charAt(a) adiciona o caractere atual da string texto2 ao HTML do elemento p.
+
+        a++ incrementa a variável a em 1.
+
+        setTimeout(animacaoH2, velo) define um atraso de velo milissegundos antes de chamar a função animacaoH2 novamente.
+
+        animacaoH1() chama a função animacaoH1 para iniciar a animação do elemento h1.
+        
+        animacaoH2() chama a função animacaoH2 para iniciar a animação do elemento p.
+
+*/
